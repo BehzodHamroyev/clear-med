f0cf368cee899251ad156dfd5e7202633b896f1c
Jasurbek Isokov <jasurisoqov685@gmail.com>
2024-01-22 19:22:30 +0500

    Finished Doctors Reports and Reports Details for Reception

--- src/entities/ButtonNavbar/model/types/ButtonNavbarTypes.ts
+++ src/entities/ButtonNavbar/model/types/ButtonNavbarTypes.ts
@@ -6,4 +6,5 @@ export interface ButtonNavbarProps {
   CreateCarbonAdd?: boolean;
   roomNumber?: number;
   departmentName?: string;
+  doctorName?: string;
 }
--- src/entities/TableTitlePatients/ui/TableTitlePatients.tsx
+++ src/entities/TableTitlePatients/ui/TableTitlePatients.tsx
@@ -1,56 +1,135 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import { useTranslation } from 'react-i18next';
 import cls from './TableTitlePatients.module.scss';
-import { TableBody, TableInfoPatients } from '../model/types/TableInfo';
+
+import { CheckedIcon, ErrorIcon } from '@/shared/assets/Pages/Doctor';
+
+interface reportDetailListTtype {
+  _id: string;
+  department_id: string;
+  room_id: string;
+  doctor_id: string;
+  queues_name: string;
+  step: number;
+  status: string;
+  created_date: string;
+  created_time: string;
+  __v: 0;
+  accepted_date: string;
+  completed_date: string;
+  id: string;
+}
+
+interface TableInfoPatients {
+  cursor?: boolean;
+  Tablethead: string[];
+  TableBody: reportDetailListTtype[];
+}
+
+const tableTitle = [
+  'ID',
+  'Qabul kuni',
+  'Qabul boshlanishi',
+  'Qabul tugashi',
+  'Xolati',
+];
 
 const TableTitlePatients = (props: TableInfoPatients) => {
   const { Tablethead, TableBody, cursor } = props;
 
-  const param: { id?: string } = useParams();
-  const idUrl = Number(param?.id);
+  const navigate = useNavigate();
 
   const { t } = useTranslation();
 
+  const handleClickBack = () => {
+    navigate('/reports');
+  };
+
   return (
     <table className={cls.TableTitleWrapper}>
-      <h3 className={cls.KorilganBemorlar}>
-        {t("Jami Ko'rilgan Bemorlar : ")}
-        {TableBody.map((item) =>
-          item.id === idUrl ? item.KorilganBemorlar?.length : null,
-        )}
-        {t(' ta')}
-      </h3>
+      {TableBody && TableBody.length > 0 ? (
+        <>
+          <div className={cls.TableTitleWrapper__head}>
+            <div
+              className={cls.TableTitleWrapper__backbtn}
+              onClick={() => handleClickBack()}
+            >
+              Ortga qaytish
+            </div>
 
-      <thead className={cls.Tablethead}>
-        <tr className={cls.tr}>
-          {Tablethead.map((title: string) => (
-            <th className={cls.th}>{title}</th>
-          ))}
-        </tr>
-      </thead>
+            <div className={cls.TableTitleWrapper__title}>
+              <h3 className={cls.KorilganBemorlar}>
+                {t("Jami Ko'rilgan Bemorlar : ")}
+                {TableBody ? TableBody.length : 0}
+                {t(' ta')}
+              </h3>
 
-      <tbody className={cls.Tabletbody}>
-        {TableBody.map((item: TableBody) =>
-          item.id === idUrl
-            ? item.KorilganBemorlar
-              ? item.KorilganBemorlar.map((bemorItem) => (
-                  <tr
-                    key={bemorItem.id}
-                    className={`${cls.tr} ${cursor ? cls.clicked : ''}`}
-                  >
-                    <td className={cls.td}>{bemorItem.id}</td>
-                    <td className={cls.td}>{bemorItem.shifokor}</td>
-                    <td className={cls.td}>{bemorItem.xona}</td>
-                    <td className={cls.td}>{bemorItem.qabulboshlanishi}</td>
-                    <td className={cls.td}>{bemorItem.qabultugashi}</td>
-                  </tr>
-                ))
-              : null
-            : null,
-        )}
-      </tbody>
+              <h3 className={cls.KorilganBemorlar}>
+                {t('Tasdiqlangan Bemorlar : ')}
+                {TableBody
+                  ? TableBody.filter((item) => item.status === 'completed')
+                      .length
+                  : 0}
+                {t(' ta')}
+              </h3>
+
+              <h3 className={cls.KorilganBemorlar}>
+                {t('Bekor qilingan Bemorlar : ')}
+                {TableBody
+                  ? TableBody.filter((item) => item.status === 'rejected')
+                      .length
+                  : 0}
+                {t(' ta')}
+              </h3>
+            </div>
+          </div>
+
+          <thead className={cls.Tablethead}>
+            <tr className={cls.tr}>
+              {tableTitle.map((title: string) => (
+                <th key={title} className={cls.th}>
+                  {title}
+                </th>
+              ))}
+            </tr>
+          </thead>
+
+          <tbody className={cls.Tabletbody}>
+            {TableBody &&
+              TableBody.length > 0 &&
+              TableBody.map((item, index) => (
+                <tr key={item.id} className={cls.tr}>
+                  <td className={cls.td}>{item.queues_name}</td>
+                  <td className={cls.td}>
+                    {item.completed_date?.split('T')[0]}
+                  </td>
+                  <td className={cls.td}>
+                    {item.accepted_date?.split('T')[1].split('.')[0]}
+                  </td>
+                  <td className={cls.td}>
+                    {item.completed_date?.split('T')[1].split('.')[0]}
+                  </td>
+                  <td className={cls.td}>
+                    <img
+                      src={
+                        item.status === 'completed' ? CheckedIcon : ErrorIcon
+                      }
+                      alt="rejected"
+                    />
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </>
+      ) : (
+        <h2 className={cls.reportEmpty}>
+          {t(
+            "Bu muddatdagi bemorlar hisoboti bo'sh. Muddatni o'zgartirib ko'ring!",
+          )}
+        </h2>
+      )}
     </table>
   );
 };
