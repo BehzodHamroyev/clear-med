400fa60880808379180631fcc9af7a4dec02ba0e
Jasurbek Isokov <jasurisoqov685@gmail.com>
2024-02-15 16:31:31 +0500

    only doctor, department and room attached rooms can be queued

--- src/pages/QueuingTV/ui/QueuingTv.tsx
+++ src/pages/QueuingTV/ui/QueuingTv.tsx
@@ -68,17 +68,26 @@ const QueuingTv = () => {
 
       <div className={cls.RenderSectionCard}>
         {deparmentList &&
-          deparmentList.map((item) => (
-            <QueuingTvCard
-              key={item?.id}
-              DoctorId={item?.doctor_id?.id}
-              CardLeftTitle={item?.department_id?.name}
-              CardLeftRoomNumber={item?.name}
-              CardLeftDoctorName={item?.doctor_id?.name}
-              // @ts-ignore
-              icon={item?.department_id?.photo}
-            />
-          ))}
+          deparmentList
+            .filter(
+              (item) =>
+                item.name &&
+                item.department_id &&
+                item.department_id._id &&
+                item.doctor_id &&
+                item.doctor_id._id,
+            )
+            .map((item) => (
+              <QueuingTvCard
+                key={item.id}
+                DoctorId={item.doctor_id.id}
+                CardLeftTitle={item.department_id.name}
+                CardLeftRoomNumber={item.name}
+                CardLeftDoctorName={item.doctor_id.name}
+                // @ts-ignore
+                icon={item.department_id.photo}
+              />
+            ))}
       </div>
 
       {isOpenQueuingTvCardPopapSecond &&
