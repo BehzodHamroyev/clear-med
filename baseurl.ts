049bb1e3fe088582abc1388aeb4292d978bf8d05
BehzodHamroyev <behzodhamroyev007@gmail.com>
2024-07-29 16:36:47 +0500

    change baseurl

--- baseurl.ts
+++ baseurl.ts
@@ -1,3 +1,4 @@
-export const baseUrl = 'http://medapi.magicsoft.uz/api/v1';
+// export const baseUrl = 'http://medapi.magicsoft.uz/api/v1';
+export const baseUrl = 'https://socketmed.magicsoft.uz/';
 
 export const baseUrlUpload = 'http://medapi.magicsoft.uz';
