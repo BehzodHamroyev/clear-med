34c446f1bf725d2acb8c2c5d7391de312b0dadcb
MJafar777 <mirzaraximovjafar2001@gmail.com>
2024-08-02 18:14:37 +0500

    s

--- src/shared/ui/QueuingTvCard/QueuingTvCardPopapSecond/ui/QueuingTvCardPopapSecond.tsx
+++ src/shared/ui/QueuingTvCard/QueuingTvCardPopapSecond/ui/QueuingTvCardPopapSecond.tsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
+import { useReactToPrint } from 'react-to-print';
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
@@ -8,13 +9,11 @@ import cls2 from './PrintQueuePage.module.scss';
 import cls from './QueuingTvCardPopapSecond.module.scss';
 import { ButtonsContext } from '@/shared/lib/context/ButtonsContext';
 import { QueueUserDoctor } from '../../../DoctorPanels/QueueUserDoctor';
-// eslint-disable-next-line ulbi-tv-plugin/public-api-imports
 import { getLastQueueData } from '@/pages/QueuingTV/model/selectors/lastQueueSelector';
 
 import { Loader } from '@/widgets/Loader';
 import { baseUrl } from '../../../../../../baseurl';
 import ErrorDialog from '../../../ErrorDialog/ErrorDialog';
-// eslint-disable-next-line ulbi-tv-plugin/public-api-imports
 import { useLasQueueActions } from '@/pages/QueuingTV/model/slice/lastQueueSlice';
 import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch/useAppDispatch';
 import {
@@ -56,6 +55,14 @@ const QueuingTvCardPopapSecond = ({
 
   const { setIsOpenQueuingTvCardPopapSecond } = useContext(ButtonsContext);
 
+  const printCom = useReactToPrint({
+    documentTitle: 'queue-data',
+    content: () => printableDivRef?.current,
+    onAfterPrint: () => {
+      setIsOpenQueuingTvCardPopapSecond(false);
+    },
+  });
+
   const createQueueFunc = async () => {
     setCreateQueueIsLoading(true);
 
@@ -78,7 +85,6 @@ const QueuingTvCardPopapSecond = ({
 
       if (response.data) {
         setCreateQueueIsLoading(false);
-
         setCreateQueueIsError(false);
 
         setPrintRoomInfo({
@@ -87,13 +93,12 @@ const QueuingTvCardPopapSecond = ({
         });
 
         setTimeout(() => {
-          window.print();
+          printCom();
           clearLastQueue();
         }, 1);
       }
     } catch (error) {
       setCreateQueueIsLoading(false);
-
       setCreateQueueIsError(true);
     }
   };
@@ -213,227 +218,3 @@ const QueuingTvCardPopapSecond = ({
 };
 
 export default QueuingTvCardPopapSecond;
-
-// import React, { useContext, useEffect, useRef, useState } from 'react';
-// import { useSelector } from 'react-redux';
-// import { useTranslation } from 'react-i18next';
-// import { useReactToPrint } from 'react-to-print';
-// import axios from 'axios';
-// import Cookies from 'js-cookie';
-
-// import cls2 from './PrintQueuePage.module.scss';
-// import cls from './QueuingTvCardPopapSecond.module.scss';
-// import { ButtonsContext } from '@/shared/lib/context/ButtonsContext';
-// import { QueueUserDoctor } from '../../../DoctorPanels/QueueUserDoctor';
-// // eslint-disable-next-line ulbi-tv-plugin/public-api-imports
-// import { getLastQueueData } from '@/pages/QueuingTV/model/selectors/lastQueueSelector';
-
-// import { Loader } from '@/widgets/Loader';
-// import { baseUrl } from '../../../../../../baseurl';
-// import ErrorDialog from '../../../ErrorDialog/ErrorDialog';
-// // eslint-disable-next-line ulbi-tv-plugin/public-api-imports
-// import { useLasQueueActions } from '@/pages/QueuingTV/model/slice/lastQueueSlice';
-// import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch/useAppDispatch';
-// import {
-//   error,
-//   isLoading,
-//   getInfoProject,
-//   getAllDataProject,
-// } from '@/entities/FileUploader';
-
-// interface QueuingTvCardPopapSecondProps {
-//   roomNumber: string;
-//   ticketNumber: string;
-// }
-
-// const QueuingTvCardPopapSecond = ({
-//   roomNumber,
-//   ticketNumber,
-// }: QueuingTvCardPopapSecondProps) => {
-//   const { t } = useTranslation();
-
-//   const dispatch = useAppDispatch();
-
-//   const printableDivRef = useRef<HTMLDivElement>(null);
-
-//   const { clearLastQueue } = useLasQueueActions();
-
-//   const [createQueueIsError, setCreateQueueIsError] = useState(false);
-//   const [createQueueIsLoading, setCreateQueueIsLoading] = useState(false);
-
-//   const [printRoomInfo, setPrintRoomInfo] = useState({
-//     createRoomNumber: roomNumber,
-//     createTicketNumber: ticketNumber,
-//   });
-
-//   const infoProjectError = useSelector(error);
-//   const lastQueue = useSelector(getLastQueueData);
-//   const infoProject = useSelector(getInfoProject);
-//   const infoProjectIsLoader = useSelector(isLoading);
-
-//   const { setIsOpenQueuingTvCardPopapSecond } = useContext(ButtonsContext);
-
-//   const printCom = useReactToPrint({
-//     content: () => printableDivRef!.current,
-//     documentTitle: 'queue-data',
-//     onAfterPrint: () => setIsOpenQueuingTvCardPopapSecond(false),
-//   });
-
-//   const createQueueFunc = async () => {
-//     setCreateQueueIsLoading(true);
-
-//     const getTokenCookie = Cookies.get('token');
-
-//     try {
-//       const response = await axios.post(
-//         `${baseUrl}/queue/create`,
-//         {
-//           department_id: lastQueue?.room.department_id,
-//           room_id: lastQueue?.room._id,
-//         },
-//         {
-//           headers: {
-//             'Content-Type': 'application/json',
-//             authorization: `Bearer ${getTokenCookie}`,
-//           },
-//         },
-//       );
-
-//       if (response.data) {
-//         setCreateQueueIsLoading(false);
-
-//         setCreateQueueIsError(false);
-
-//         setPrintRoomInfo({
-//           createRoomNumber: String(response.data?.room?.name),
-//           createTicketNumber: String(response.data?.navbat?.queues_name),
-//         });
-
-//         setTimeout(() => {
-//           printCom();
-
-//           clearLastQueue();
-//         }, 1);
-//       }
-//     } catch (error) {
-//       setCreateQueueIsLoading(false);
-
-//       setCreateQueueIsError(true);
-//     }
-//   };
-
-//   const handlePrint = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-//     e.stopPropagation();
-
-//     if (
-//       lastQueue &&
-//       (lastQueue.data.department_id || lastQueue.room.department_id) &&
-//       printableDivRef?.current
-//     ) {
-//       createQueueFunc();
-//     }
-//   };
-
-//   useEffect(() => {
-//     dispatch(getAllDataProject({}));
-//   }, [dispatch]);
-
-//   const imgLink: string = `http://socketmed.magicsoft.uz//${infoProject?.[0]?.logo}`;
-
-//   return (
-//     <div className={cls.QueuingTvCardPopapSecondWrapper}>
-//       {!createQueueIsLoading && infoProject && (
-//         <div className={cls.QueuingTvCardPopapSecond}>
-//           <h3 className={cls.QueuingTvCardPopapSecondTitle}>
-//             {t('Navbatni tasdiqlang')}
-//           </h3>
-
-//           <div ref={printableDivRef} className={cls.QueuingTvPrintCard}>
-//             <div className={cls2.PrintQueuePage}>
-//               <img
-//                 src={imgLink}
-//                 alt="imgLink"
-//                 className={cls.PrintQueuePage__img}
-//               />
-
-//               <div className={cls2.PrintQueuePage__queueBox}>
-//                 <QueueUserDoctor
-//                   ticketNumber={printRoomInfo.createTicketNumber}
-//                   roomNumber={printRoomInfo.createRoomNumber}
-//                 />
-//               </div>
-
-//               <div className={cls2.PrintQueuePage__medicName}>
-//                 <p>Бўлим:</p>
-//                 <p className={cls2.medicNameDeparment}>
-//                   {lastQueue?.data?.department_id
-//                     ? lastQueue?.data?.department_id?.name
-//                     : lastQueue?.room?.department_id?.name}
-//                 </p>
-//               </div>
-
-//               <div className={cls2.PrintQueuePage__medicName}>
-//                 <p>Шифокор:</p>
-//                 <p className={cls2.medicNameFullName}>
-//                   {lastQueue?.data?.doctor_id
-//                     ? lastQueue?.data?.doctor_id?.name
-//                     : lastQueue?.room?.doctor_id?.name}
-//                 </p>
-//               </div>
-
-//               <div className={cls2.PrintQueuePage__medicName}>
-//                 <p>Берилган вақт:</p>
-//                 <p className={cls2.PrintQueuePage__dateGetQueue}>
-//                   {new Date().getDate()}/
-//                   {new Date().getMonth() < 10
-//                     ? `0${new Date().getMonth() + 1}`
-//                     : new Date().getMonth() + 1}
-//                   /{new Date().getFullYear()} |{' '}
-//                   {new Date().getHours() < 10
-//                     ? `0${new Date().getHours()}`
-//                     : new Date().getHours()}
-//                   :
-//                   {new Date().getMinutes() < 10
-//                     ? `0${new Date().getMinutes()}`
-//                     : new Date().getMinutes()}
-//                 </p>
-//               </div>
-
-//               <p className={cls2.PrintQueuePage__message}>
-//                 Ташрифингиз учун раҳмат!
-//               </p>
-//             </div>
-//           </div>
-
-//           <div className={cls.BtnParnet}>
-//             <button
-//               onClick={() => {
-//                 setIsOpenQueuingTvCardPopapSecond(false);
-//               }}
-//               type="button"
-//               className={`${cls.Btn} ${cls.Btn1}`}
-//             >
-//               {t('Bekor qilish')}
-//             </button>
-
-//             <button
-//               onClick={(e) => handlePrint(e)}
-//               type="button"
-//               className={`${cls.Btn} ${cls.Btn2}`}
-//             >
-//               {t('Chiqarish')}
-//             </button>
-//           </div>
-//         </div>
-//       )}
-
-//       {createQueueIsLoading && infoProjectIsLoader && <Loader />}
-
-//       {createQueueIsError && infoProjectError && (
-//         <ErrorDialog isErrorProps={!false} />
-//       )}
-//     </div>
-//   );
-// };
-
-// export default QueuingTvCardPopapSecond;
