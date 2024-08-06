3160af2d1badf226e397037b7ac18670bb579053
BehzodHamroyev <behzodhamroyev007@gmail.com>
2024-08-06 16:05:58 +0500

    add multiple doctor to room

--- src/pages/AddRoomPage/ui/AddRoomPage.tsx
+++ src/pages/AddRoomPage/ui/AddRoomPage.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React, { useContext, useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
