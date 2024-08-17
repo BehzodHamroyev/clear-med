c54fb59e5ec46f195997e1c4c7fa00e872840139
plux96 <rustamov.umidbek@gmail.com>
2024-08-17 09:39:35 +0500

    s

--- baseurl.ts
+++ baseurl.ts
@@ -1,11 +1,13 @@
 // export const baseUrl = 'http://medapi.magicsoft.uz/api/v1';
-// export const baseUrl = 'http://192.168.1.130:3009/api/v1';
-export const baseUrl = 'https://socketmed.magicsoft.uz//api/v1';
+export const baseUrl = 'http://192.168.0.130:3009/api/v1';
+// export const baseUrl = 'https://socketmed.magicsoft.uz//api/v1';
 
 // export const baseUrlUpload = 'http://socketmed.magicsoft.uz/';
 
 // export const baseUrl = 'http://socketmed.magicsoft.uz//api/v1';
 
-export const baseUrlImgLogo = 'http://socketmed.magicsoft.uz/';
+// export const baseUrlImgLogo = 'http://socketmed.magicsoft.uz/';
+export const baseUrlImgLogo = 'http://192.168.0.130:3009/api/v1';
 
+// export const baseUrlUpload = 'http://192.168.1.130:3009/api/v1';
 export const baseUrlUpload = 'https://socketmed.magicsoft.uz/';
